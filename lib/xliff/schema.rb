077e34dfcf25c840ba9f0f9f4a163a3126f74a43
Martin Tepper <mt@promptus-partners.de>
2016-10-06 16:31:28 +0200

    Put local XML Schema xsd file into project, and use it in the xliff schema documents so Nokogiri doesn't download the same linked file time and time again

--- lib/xliff/schema.rb
+++ lib/xliff/schema.rb
@@ -6,6 +6,9 @@ module Xliff
         V1_2_STRICT_LOCATION = 'urn:oasis:names:tc:xliff:document:1.2 xliff-core-1.2-strict.xsd'
         V1_2_TRANSITIONAL_LOCATION = 'urn:oasis:names:tc:xliff:document:1.2 xliff-core-1.2-transitional.xsd'
 
+        # Hack local path into schema files so Nokokiri doesn't download the linked file
+        XML_NAMESPACE_PATH = File.dirname(__FILE__)+'/../../xsd/xml-namespace.xsd'
+
         def self.get_strict_schema
             self.get_schema(:strict)
         end
@@ -13,7 +16,9 @@ module Xliff
             self.get_schema(:transitional)
         end
         def self.get_schema(type)
-            Nokogiri::XML::Schema(File.read((type == :transitional ? self::V1_2_TRANSITIONAL : self::V1_2_STRICT)))
+            schema_xml = File.read((type == :transitional ? self::V1_2_TRANSITIONAL : self::V1_2_STRICT))
+            schema_xml.gsub!('XML_SCHEMA_LOCATION_PLACEHOLDER', XML_NAMESPACE_PATH)
+            Nokogiri::XML::Schema(schema_xml)
         end
     end
 end
